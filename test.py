f5e11c7b7e5cdbcf3b6525daabb342b457535fac
YannLeo <1070032777@qq.com>
2024-10-07 05:48:37 +0800

    KMP

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
-def searchInsert(nums, target: int) -> int:
-    left,right=0,len(nums)-1
-    while left<=right:
-        mid=(left+right)//2
-        if nums[mid]<=target:
-            left=mid+1
-        else:
-            right=mid-1
-    return left
+def kmp(s, t):
+    if not t: return 0
+    pi = [0] * len(t)
+    j = 0
+    for i in range(1, len(t)):
+        while j > 0 and t[i] != t[j]:
+            j = pi[j-1]
+        if t[i] == t[j]:
+            j += 1
+        pi[i] = j
 
-nums = [3,3,3,3,4]
-target = 2
-print(searchInsert(nums,target))
+    j = 0
+    for i, c in enumerate(s):
+        while j > 0 and t[j] != c:
+            j = pi[j-1]
+        if t[j] == c:
+            j += 1
+        if j == len(t):
+            return i - j + 1
+
+    return -1
+
+if __name__ == '__main__':
+    text = "abababaabc"
+    pattern = "ababaab"
+    res = kmp(text, pattern)
+    print(res)
