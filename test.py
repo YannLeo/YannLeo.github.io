4f80116d4c29b6874d5790d0271f650f3d1fdb31
YannLeo <1070032777@qq.com>
2025-06-12 16:27:41 +0800

    添加部分新内容

--- test.py
+++ test.py
@@ -1,31 +1,13 @@
-import heapq
+N, V = map(int, input().split())
+dp = [0]*(V+1)
 
-def prim(graph, start):
-    min_spanning_tree = []
-    visited = set()
-    priority_queue = [(0, -1, start)]
-    heapq.heapify(priority_queue)
-
-    while priority_queue:
-        weight, father, node = heapq.heappop(priority_queue)
-        if node not in visited:
-            visited.add(node)
-            min_spanning_tree.append((weight, father, node))
-
-            for neighbor, neighbor_weight in graph[node].items():
-                if neighbor not in visited:
-                    heapq.heappush(priority_queue, (neighbor_weight, node, neighbor))
-
-    return min_spanning_tree
-
-
-graph = {
-    'A': {'B': 1, 'D': 4},
-    'B': {'A': 1, 'C': 2, 'E': 3},
-    'C': {'B': 2, 'F': 5},
-    'D': {'A': 4, 'E': 1},
-    'E': {'B': 3, 'D': 1, 'F': 2},
-    'F': {'C': 5, 'E': 2}
-}
-
-print("Prim算法最小生成树：", prim(graph, 'A'))
+for _ in range(N):
+    s = int(input())
+    vs, ws = [], []
+    for _ in range(s):
+        v, w = map(int, input().split())
+        vs.append(v); ws.append(w)
+    for j in range(V, 0, -1):
+        for v, w in zip(vs, ws):
+            if j >= v: dp[j] = max(dp[j], dp[j-v]+w)
+print(dp[-1])
